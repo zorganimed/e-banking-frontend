bf54a02df9436332f788435a70e1ebae91d76ef6
Zorgani <mohamed01.zorgani@gmail.com>
2023-10-17 18:02:34 +0100

    ameliorer le composant customer ainsi le service

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {CustomerService} from "../services/customer.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {Customer} from "../model/customer.model";
 
 @Component({
   selector: 'app-customers',
@@ -7,17 +10,20 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit{
-  customers:any;
+  customers!:Observable<Array<Customer>>;
 
-  constructor(private http : HttpClient) {
+  errorMessage!:string;
+
+  constructor(private customerService : CustomerService) {
   }
 
   ngOnInit(): void {
-    this.http.get("http://localhost:8085/customers").subscribe(data=>{
-      this.customers=data;
-    },err=>{
-      console.log(err);
-    })
+    this.customers = this.customerService.getCustomers().pipe(
+      catchError(err => {
+        this.errorMessage = err.message;
+        return throwError(err);
+      })
+    );
   }
 
 }
